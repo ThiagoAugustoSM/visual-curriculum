c76fa81472398ecc787d0ef14b5546edc6d26424
Thiago Augusto <martinsthiagoaugusto@gmail.com>
2021-01-15 23:38:33 -0300

    Add Card for the discipline

---
+++ src/components/DisciplineBox/index.tsx
@@ -0,0 +1,108 @@
+import React, { useState, useEffect } from 'react';
+
+import { Box, Badge, useColorMode } from '@chakra-ui/react';
+
+const badgeSemesterColor = (semester) => {
+  switch(semester){
+    case 1:
+      return "gray"
+    case 2:
+      return "red"
+    case 3:
+      return "orange"
+    case 4:
+      return "yellow"
+    case 5:
+      return "green"
+    case 6:
+      return "teal"
+    case 7:
+      return "blue"
+    case 8:
+      return "cyan"
+    case 9:
+      return "purple"
+    case 9:
+      return "pink"
+    default:
+      return "gray"
+  }
+}
+const DisciplineBox = (props) => {
+  const {semester, isObligatory, name, hours, credits, id, onClick} = props;
+  const [isActive, setIsActive] = useState(false);
+  const { colorMode, toggleColorMode } = useColorMode()
+  // const bg = useColorModeValue("red.500", "red.200")
+  const [bgColor, setBgColor] = useState("white");
+  const [bgColorDark, setBgColorDark] = useState("");
+
+  const handleClick = () => {
+    if(isActive === true){
+      setBgColor("white");
+      setBgColorDark("gray.800");
+    } else{
+      setBgColor("green.200");
+      setBgColorDark("green.500");
+    }
+    onClick({isActive: !isActive, id, isObligatory, hours})
+    setIsActive(!isActive);
+  }
+
+  return (
+    <Box 
+      id={id} 
+      onClick={handleClick} 
+      m="3"
+      minW="200px" 
+      maxW="sm" 
+      borderWidth="1px" 
+      borderRadius="lg" 
+      overflow="hidden"
+      bgColor={colorMode === "light" ? bgColor : bgColorDark }
+    >
+      <Box p="4" >
+        <Box d="flex" alignItems="baseline">
+          <Badge borderRadius="full" px="2" colorScheme={badgeSemesterColor(semester)}>
+            {semester} º Período
+          </Badge>
+          <Box
+            color="gray.500"
+            fontWeight="semibold"
+            letterSpacing="wide"
+            fontSize="xs"
+            textTransform="uppercase"
+            ml="2"
+          >
+            {isObligatory ? 'Obrigatória' : 'Eletiva'}
+          </Box>
+        </Box>
+
+        <Box
+          mt="1"
+          fontWeight="semibold"
+          as="h4"
+          lineHeight="tight"
+          isTruncated
+        >
+          {name}
+        </Box>
+
+        <Box>
+          {hours}
+          <Box as="span" color="gray.600" fontSize="sm">
+            HRs
+          </Box>
+        </Box>
+
+        <Box>
+          {credits}
+          <Box as="span" ml="1" color="gray.600" fontSize="sm">
+            crédito(s)
+          </Box>
+        </Box>
+      </Box>
+    </Box>
+  )
+}
+
+export default DisciplineBox;
