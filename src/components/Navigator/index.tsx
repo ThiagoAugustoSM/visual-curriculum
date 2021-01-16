a0a7f2e6d0da04df097bea652b2cb94c5fe3d616
Thiago Augusto <martinsthiagoaugusto@gmail.com>
2021-01-15 23:37:55 -0300

    Add page navigator

---
+++ src/components/Navigator/index.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+import {
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink
+} from '@chakra-ui/react';
+
+const Navigator = () => {
+  return (
+    <Breadcrumb size="20" m="3" separator="/">
+      <BreadcrumbItem>
+        <BreadcrumbLink href="#">UFPE
+        </BreadcrumbLink>
+      </BreadcrumbItem>
+      <BreadcrumbItem>
+        <BreadcrumbLink href="#" isCurrentPage>Engenharia da Computação</BreadcrumbLink>
+      </BreadcrumbItem>
+    </Breadcrumb>
+  );
+}
+
+export default Navigator;
