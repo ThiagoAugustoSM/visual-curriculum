b86759ac29676913c093949f93c66bc1a05ced2c
Thiago Augusto <martinsthiagoaugusto@gmail.com>
2021-01-15 23:36:34 -0300

    Add chakra provider

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { ChakraProvider, CSSReset, ColorModeScript } from "@chakra-ui/react"
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ChakraProvider>
+      <CSSReset/>
+      <ColorModeScript />
+      <App />
+    </ChakraProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
