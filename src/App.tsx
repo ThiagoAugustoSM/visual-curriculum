a6c865e0535d640183ab7b111ee0468b961dfda3
Thiago Augusto <martinsthiagoaugusto@gmail.com>
2021-01-15 23:38:45 -0300

    Add first version of App

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,94 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react';
+import { 
+  Box,
+  Stack,
+  Heading, 
+  SimpleGrid
+ } from '@chakra-ui/react';
+
+import Header from './components/Header';
+import Navigator from './components/Navigator';
+import DisciplineBox from './components/DisciplineBox';
+import StatsContainer from './components/StatsContainer';
+import NextSteps from './components/NextSteps';
+import Footer from './components/Footer';
 
 function App() {
+  const [curriculum, setCurriculum] = useState({})
+  const [academicTotalDone, setAcademicTotalDone] = useState(0)
+  const [academicObligatoryDone, setAcademicObligatoryDone] = useState(0)
+  const [academicElectiveDone, setAcademicElectiveDone] = useState(0)
+  
+  const handleClick = ({isActive, id, isObligatory, hours}) => {
+    if(isActive){
+      if(isObligatory){
+        setAcademicObligatoryDone(academicObligatoryDone + hours);
+      } else {
+        setAcademicElectiveDone(academicElectiveDone + hours);
+      }
+      setAcademicTotalDone(academicTotalDone + hours);
+    } else {
+      if(isObligatory){
+        setAcademicObligatoryDone(academicObligatoryDone - hours);
+      } else {
+        setAcademicElectiveDone(academicElectiveDone - hours);
+      }
+      setAcademicTotalDone(academicTotalDone - hours);
+    }
+  }
+  
+  useEffect(() => {
+    fetch('./university/UFPE/engenhariaDaComputacao.json')
+      .then(response => response.json())
+      .then(json => setCurriculum(json))
+  }, [])
+  
+  const arrayOfSemesters = Array.from({length: curriculum?.semesters}, (_, i) => i + 1)
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Box m="5">
+      <Header/>
+      <Navigator/>
+      <Box
+        w="100%"
+        maxH="60vh"
+        overflow="scroll"
+      >
+        {arrayOfSemesters.map(semester => (
+          <Box
+            d="flex"
+            w="100%"
+            key={`row-${semester}`}
+          >
+            {
+              curriculum?.disciplines.filter((item) => item.semester === semester).map((item) => (
+                <DisciplineBox
+                  key={item.name} 
+                  id={item.code} 
+                  onClick={handleClick}
+                  {...item} 
+                />
+              ))
+            }
+          </Box>
+        ))}
+      </Box>
+      <SimpleGrid
+        columns={[1, 2]}
+        spacing="5"
+      >
+        <StatsContainer
+          academicTotalDone={academicTotalDone}
+          academicObligatoryDone={academicObligatoryDone}
+          academicElectiveDone={academicElectiveDone}
+          totalHours={curriculum?.totalHours}
+          totalHoursObligatory={curriculum?.totalHoursObligatory}
+          totalHoursElective={curriculum?.totalHoursElective}
+        />
+        <NextSteps/>
+      </SimpleGrid>
+      <Footer/>
+    </Box>
   );
 }
 
