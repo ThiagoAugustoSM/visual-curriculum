2535b72b1f94b0d6b80c086f94a36eca23d59ddf
hggmarks <hggmarks@gmail.com>
2022-06-07 21:16:55 -0300

    added the dependents field filled

--- scrapy.py
+++ scrapy.py
@@ -1,3 +1,5 @@
+import sys, fitz
+import re, json
 HEADER_FONT_SIZE = 59.808998107910156
 
 def get_block_info(block: tuple) -> tuple:
@@ -114,7 +116,8 @@ def get_equivalence_and_prereq_values(string: str,
         else: pre_req = splitted_string[0]
         disc_prerequisites = get_equivalences(pre_req)
         is_prerequisite = False
-    return is_equivalence, is_prerequisite, disc_prerequisites, disc_equivalences
+    return (is_equivalence, is_prerequisite,
+            disc_prerequisites, disc_equivalences)
 
 def save_json_file(output_path: str, university_name: str, course_name: str,
                    total_hours: int, elective_hours: int, obligatory_hours: int,
@@ -130,19 +133,17 @@ def save_json_file(output_path: str, university_name: str, course_name: str,
             "disciplines": disciplines,
         }, file, ensure_ascii=False, indent=2)
 
-def fill_disciplines_dependents(disciplines_list: list) -> list:
-    for discipline_i in disciplines_list:
-        discipline_i["dependents"] = []
-        for discipline_j in disciplines_list:
-            if discipline_i == discipline_j:
-                continue
-            else:
-                for item in discipline_j.prerequisites:
-                    if discipline_i.code == item["code"]:
-                        discipline_i["dependents"].append({
-                            "code": discipline_j.code
-                            })
-    return discipline_i
+def get_dependents_value(discipline_list: list) -> list:
+
+    for discipline in discipline_list:
+        for item in discipline['prerequisites']:
+            for x in discipline_list:
+                if x['code'] == item['code']:
+                    x['dependents'].append({
+                        'code': discipline['code'],
+                        'name': discipline['name']
+                        })
+    return discipline_list
 
 def pdf_to_json(pdf_path: str, output_json: str):
     document = fitz.open(pdf_path)
@@ -193,6 +194,7 @@ def pdf_to_json(pdf_path: str, output_json: str):
                         "semester": current_semester,
                         "equivalences": disc_equivalences,
                         "prerequisites": disc_prerequisites,
+                        "dependents": [],
                     })
                     ementa = ""
                     disc_equivalences = []
@@ -220,8 +222,10 @@ def pdf_to_json(pdf_path: str, output_json: str):
         "semester": current_semester,
         "equivalences": disc_equivalences,
         "prerequisites": disc_prerequisites,
+        "dependents": [],
     })
-
+    
+    get_dependents_value(disciplines)
     save_json_file(output_json, university_name, course_name,
                    total_hours, elective_hours, obligatory_hours,
                    semesters, disciplines)
@@ -277,6 +281,7 @@ def pdf_to_json_2(pdf_path: str, output_json: str):
                         "isObligatory": disc_type,
                         "equivalences": disc_equivalences,
                         "prerequisites": disc_prerequisites,
+                        "dependents": [],
                     })
                     ementa = ""
                     disc_equivalences = []
@@ -306,11 +311,16 @@ def pdf_to_json_2(pdf_path: str, output_json: str):
         "isObligatory": disc_type,
         "equivalences": disc_equivalences,
         "prerequisites": disc_prerequisites,
+        "dependents": [],
     })
+    get_dependents_value(disciplines)
     print(disciplines)   
     save_json_file(output_json, university_name, course_name,
                    total_hours, elective_hours, obligatory_hours,
                    semesters, disciplines)
 
 if __name__ == "__main__":
-    pdf_to_json_2(sys.argv[1], sys.argv[2])
+    if sys.argv[1] in ['cc', 'si']:
+        pdf_to_json(sys.argv[2], sys.argv[3])
+    elif sys.argv[1] == 'ec':
+        pdf_to_json_2(sys.argv[2], sys.argv[3])
