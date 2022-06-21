7a54e3e3d97f130d53faa3bba81e62a46ebea661
José Daniel <jdsc2@cin.ufpe.br>
2022-06-21 03:24:35 -0300

    FEAT: Update links in detail

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
-
-import { Stack, Text, Icon, IconButton } from '@chakra-ui/react';
+import { Box, Stack, Text, Icon, IconButton } from '@chakra-ui/react';
 import { FaLinkedinIn, FaGithub } from 'react-icons/fa';
+import { DiGitMerge } from 'react-icons/di';
+import abagatinhoIcon from '../../assets/img/abagatinho.svg';
 
 const Footer = (): React.ReactElement => {
   return (
@@ -11,19 +12,44 @@ const Footer = (): React.ReactElement => {
         <span role="img" aria-label="hearth">
           💙
         </span>{' '}
-        em Recife, por Thiago Augusto
+        em Recife, por Thiago Augusto <Icon color="green" as={DiGitMerge} />{' '}
+        Abagatinho
       </Text>
       <Stack spacing={3} direction="row">
-        <IconButton
-          size="sm"
-          aria-label="Abrir Linkedin"
-          icon={<Icon as={FaLinkedinIn} />}
-        />
-        <IconButton
-          size="sm"
-          aria-label="Abrir Linkedin"
-          icon={<Icon as={FaGithub} />}
-        />
+        <a href="https://www.linkedin.com/in/thiagoaugustosm/">
+          <IconButton
+            size="sm"
+            aria-label="Abrir Linkedin"
+            icon={<Icon as={FaLinkedinIn} />}
+          />
+        </a>
+        <a href="https://github.com/ThiagoAugustoSM">
+          <IconButton
+            size="sm"
+            aria-label="Abrir Linkedin"
+            icon={<Icon as={FaGithub} />}
+          />
+        </a>
+
+        <Box
+          bg="var(--chakra-colors-gray-100)"
+          padding="0px"
+          lineHeight="1.2"
+          justifyContent="center"
+          borderRadius="var(--chakra-radii-md)"
+          display="inline-flex"
+          outlineOffset="2px"
+          whiteSpace="nowrap"
+          position="relative"
+          width="var(--chakra-sizes-8)"
+          font-size="var(--chakra-fontSizes-sm)"
+          padding-inline-start="var(--chakra-space-3)"
+          cursor="pointer"
+        >
+          <a href={'https://github.com/Abagatinho'}>
+            <img src={abagatinhoIcon} alt="Abrir organização" />
+          </a>
+        </Box>
       </Stack>
     </Stack>
   );
