e983b51c4b4d33762b62e296bfc121f6ba597ee2
José Daniel <jdsc2@cin.ufpe.br>
2022-06-23 01:43:21 -0300

    Remove typescript-eslint in index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,7 @@ import App from './App';
 
 import { createRoot } from 'react-dom/client';
 const container = document.getElementById('root');
+// eslint-disable-next-line @typescript-eslint/no-non-null-assertion
 const root = createRoot(container!); // createRoot(container!) if you use TypeScript
 
 root.render(
